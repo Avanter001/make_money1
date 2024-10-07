6d4e9150576f9c46f9e41687acfc3f106ef2ab6b
Avanter001 <79634075+Avanter001@users.noreply.github.com>
2024-10-07 19:30:14 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch'); // Import node-fetch
+const keep_alive = require('./keep_alive.js')
 const url = "https://api.adsgram.ai/event?type=reward&trackingtypeid=14&record=IiQ4NDdmNzg2ZS0zMGVlLTQwNDYtOGQ4Ny03MWU3MWM4ZTMwODEqAzE0NzIDMTU0OgY2MTQwMzVAjP6PuAZKCjEwNzk1MDM0MjFSBDM4NTJaGmh0dHBzOi8vYm90LnRvbmNpcmNsZS5vcmcvYgExagR3ZWJhcgJlbooBAjE0kgEENzI2NJoBDTMwNzAxNy41MDAwMDCqAQgxLjAwMDAwMLIBATa6AQs5NC4yNDIuNTAuOcIBAnJ1";
 
 function makeMoney() {
